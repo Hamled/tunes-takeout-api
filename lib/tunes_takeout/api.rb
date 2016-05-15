b821f8b051d5286bab8f3eab7a06c281915005ba
Charles Ellis <hamled@hamled.name>
2016-05-15 16:44:35 -0700

    Search request

--- lib/tunes_takeout/api.rb
+++ lib/tunes_takeout/api.rb
@@ -1,14 +1,42 @@
 require 'sinatra/base'
 require 'sinatra/namespace'
 require 'sinatra/json'
+require 'uri'
 
 module TunesTakeout
   class API < Sinatra::Base
     register Sinatra::Namespace
 
+    DEFAULT_LIMIT = 10
+
     namespace '/v1' do
       get '/ping' do
         json data: 'pong'
+      end
+
+      get '/search' do
+        query = params['query']
+        limit = params['limit'].to_i || DEFAULT_LIMIT
+        seed = params['seed'] || query
+
+        suggestions = Suggestion.search(query, limit, seed)
+
+        json({
+          href: canonical_url({
+            query: query,
+            limit: limit,
+            seed: seed
+          }),
+          suggestions: suggestions
+        })
+      end
+
+      helpers do
+        def canonical_url(params)
+          url = URI(request.url)
+          url.query = URI.encode_www_form(params)
+          return url
+        end
       end
     end
   end
